fbe86107fd590a1b76658ff02a1c9dd8676d5b02
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-03-29 12:19:43 -0300

    Primeiro commit

---
+++ frontend/src/axios.js
@@ -0,0 +1,11 @@
+// src/axios.js
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosInstance;
