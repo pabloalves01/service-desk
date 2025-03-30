e102a5a732d161bf942b71160dde2b40653d26be
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-03-30 11:56:36 -0300

    Added new sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,46 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Sidebar from './components/comum/sidebar.vue'
 
 const message = ref('')
+const errorMessage = ref('')
 
-onMounted(() => {
-  // Fazendo a requisição GET para a API Laravel
-  axios.get('http://localhost:8000/api/test')  // URL do backend Laravel
-    .then(response => {
-      message.value = response.data.message  // A resposta será exibida no template
-    })
-    .catch(error => {
-      console.error('Erro ao fazer a requisição:', error)
-    })
-})
+const fetchMessage = async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/api/test')
+    message.value = response.data.message
+  } catch (error) {
+    console.error('Erro ao fazer a requisição:', error)
+    errorMessage.value = 'Erro ao carregar a mensagem'
+  }
+}
+
+onMounted(fetchMessage)
 </script>
 
 <template>
-  <main>
-    <router-view></router-view>
-  </main>
+  <div class="container">
+    <Sidebar />
+    <main>
+      <router-view />
+      <p v-if="message">{{ message }}</p>
+      <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+    </main>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+}
 
+main {
+  flex: 1;
+  padding: 1rem;
+}
 
+.error {
+  color: red;
+}
 </style>
